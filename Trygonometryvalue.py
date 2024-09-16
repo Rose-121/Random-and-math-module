b1aba82a76f190faa5aae568d6242a527c4ac2e0
Rose <mehrabkhanrose10@gmail.com>
2024-09-16 22:22:15 +0600

    roseee

---
+++ Trygonometryvalue.py
@@ -0,0 +1,10 @@
+import math
+x = float(input("Enter your number:"))
+
+print(math.sin(x))
+print(math.cos(x))
+print(math.tan(x))
+
+print(math.ceil(x))
+print(math.floor(x))
+print(math.factorial(x))
